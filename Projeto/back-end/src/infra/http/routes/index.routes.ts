769f9d4731c00c9a3174576898ffd46497b3cd9c
zlimaz <miguell1040z@gmail.com>
2024-08-26 23:26:18 -0300

    Endpoint de Constantes #80

--- Projeto/back-end/src/infra/http/routes/index.routes.ts
+++ Projeto/back-end/src/infra/http/routes/index.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userRoutes from "../../../modules/users/infra/routes/user.routes";
 import teamsRoutes from "../../../modules/teams/infra/routes/teams.routes";
+import modalities from "../../../constants/modalities";
 
 const routes = Router();
 
 routes.use('/users', userRoutes);
 routes.use('/teams', teamsRoutes);
+routes.get('/constants/modalities', (_, res) => res.json(modalities));
 
 routes.get('/', (_, res) => res.send('Hello, world!'));
 
